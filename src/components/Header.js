c0e25f2c9ffe5cc702a4fcb365606ac7bd78a664
joshuaramat <ramatjoshua@gmail.com>
2022-07-28 18:23:40 -0700

    cleaning up design

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,9 +11,11 @@ function Header() {
   const cars = useSelector(selectCars);
   return (
     <Container>
-      <a>
-        <img src="/images/logo.svg" alt=""></img>
-      </a>
+      <HeaderLogo>
+        <img 
+          src='https://assets.website-files.com/5e8fceb1c9af5c3915ec97a0/5ec2f037975ed372da9f6286_Tesla-Logo-PNG-HD.png' alt='' 
+        />
+      </HeaderLogo>
       <Menu>
         {/* uses Redux to map over the car array that have car names */}
         {cars && cars.map((car, index) => (
@@ -23,9 +25,11 @@ function Header() {
       </Menu>
       <RightMenu>
         <a href="#">Shop</a>
-        <a href="#">Tesla Account</a>
+        <a href="#">Account</a>
         {/* opens the burger menu on the right side when clicked */}
-        <CustomMenu onClick={() => setBurgerStatus(true)} />
+        <CustomMenu onClick={() => setBurgerStatus(true)}>
+          Menu
+        </CustomMenu>
       </RightMenu>
 
       <BurgerNav show={burgerStatus}>
@@ -38,12 +42,24 @@ function Header() {
           <li key={index}><a href="#">{car}</a></li>
 
         ))}
+        <li><a href="#">Solar Roof</a></li>
+        <li><a href="#">Solar Panels</a></li>
         <li><a href="#">Existing Inventory</a></li>
         <li><a href="#">Used Inventory</a></li>
         <li><a href="#">Trade-In</a></li>
-        <li><a href="#">Cybertruck</a></li>
-        <li><a href="#">Roadster</a></li>
-
+        <li><a href="#">Test Drive</a></li>
+        <li><a href="#">Insurance</a></li>
+        <li><a href="#">Powerwall</a></li>
+        <li><a href="#">Commercial Energy</a></li>
+        <li><a href="#">Utilities</a></li>
+        <li><a href="#">Charging</a></li>
+        <li><a href="#">Find Us</a></li>
+        <li><a href="#">Support</a></li>
+        <li><a href="#">Investor Relations</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">Account</a></li>
+        <li><a href="#">More</a></li>
+        <li><a href="#">United States</a></li>
       </BurgerNav>
     </Container>
   )
@@ -64,6 +80,15 @@ const Container = styled.div`
   z-index: 1;
 `
 
+const HeaderLogo = styled.div`
+  img {
+    object-fit: contain;
+    flex: 1;
+    width: 100px;
+    margin-right: 100px;
+  }
+`
+
 const Menu = styled.div`
   display: flex;
   align-items: center;
@@ -71,8 +96,7 @@ const Menu = styled.div`
   flex: 1;
 
   a{
-    font-weight:600;
-    text-transform:uppercase;
+    font-weight:400;
     padding: 0 10px;
     flex-wrap: no-wrap;
   }
@@ -86,15 +110,18 @@ const RightMenu = styled.div`
   display:flex;
   align-items:center;
   a{
-    font-weight:600;
-    text-transform:uppercase;
+    font-weight:400;
     margin-right: 10px;
+  }
 
-
+  @media(max-width: 768px){
+    a {
+      display:none;
+    }
   }
 `
 
-const CustomMenu = styled(MenuIcon)`
+const CustomMenu = styled.div`
   cursor: pointer;
 `
 //smaller z-index to move over container
@@ -102,6 +129,7 @@ const CustomMenu = styled(MenuIcon)`
 
 const BurgerNav = styled.div`
   position: fixed;
+  overflow-y: scroll;
   top: 0;
   bottom 0;
   right: 0;
@@ -115,12 +143,13 @@ const BurgerNav = styled.div`
   text-align: start;
   transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
   transition: transform 0.2s;
+  margin-right: 0;
   li{
     padding: 15px 0px;
     border-bottom: 1px solid rgba(0,0,0, .2);
 
     a {
-      font-weight: 600;
+      font-weight: 400;
     }
   }
 `
